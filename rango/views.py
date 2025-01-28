32313bc19ded81d3e4e2c2ebf083d8c91a35d73d
Elias Koch <3090665K@student.gla.ac.uk>
2025-01-28 00:41:56 +0000

    finished chapter 4 exercises

--- rango/views.py
+++ rango/views.py
@@ -13,4 +13,4 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("<p>Rango says here is the about page.</p><p><a href='/rango/'>Index</a>")
+    return render(request, 'rango/about.html')
